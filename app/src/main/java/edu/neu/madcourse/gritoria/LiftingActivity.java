6f3f0e13800054bc0074a7c6e53cf1d9afe30ad2
Alex <thammavongsa.a@northeastern.edu>
2021-03-16 19:21:51 -0400

    saving

---
+++ app/src/main/java/edu/neu/madcourse/gritoria/LiftingActivity.java
@@ -0,0 +1,13 @@
+package edu.neu.madcourse.gritoria;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LiftingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
