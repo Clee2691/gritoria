0024d5a6e3abfbf89411fbdd1d1b1aca1453f503
Alex <thammavongsa.a@northeastern.edu>
2021-03-16 20:36:16 -0400

    added progress

--- app/src/main/java/edu/neu/madcourse/gritoria/LiftingActivity.java
+++ app/src/main/java/edu/neu/madcourse/gritoria/LiftingActivity.java
@@ -1,6 +1,9 @@
 package edu.neu.madcourse.gritoria;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,24 @@ public class LiftingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lifting);
+
+        Button progress = findViewById(R.id.progressButton);
+        progress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProgress();
+            }
+        });
     }
+
+    public void openProgress(){
+        Intent intent = new Intent(this, Progress.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
 }
