c0acf1266011dc02683e0aec0864993fa3f4b742
Alex <thammavongsa.a@northeastern.edu>
2021-03-16 21:04:58 -0400

    commiting changes

--- app/src/main/java/edu/neu/madcourse/gritoria/LiftLog.java
+++ app/src/main/java/edu/neu/madcourse/gritoria/LiftLog.java
@@ -1,13 +1,27 @@
 package edu.neu.madcourse.gritoria;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 
 public class LiftLog extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RunningLogAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liftlog);
     }
+
+
+
 }
