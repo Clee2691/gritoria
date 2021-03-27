beb773664542cb580991429f37a75f3a00843a86
Alex <thammavongsa.a@northeastern.edu>
2021-03-27 14:58:21 -0400

    committing log card

---
+++ app/src/main/java/edu/neu/madcourse/gritoria/LiftItemCard.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.gritoria;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LiftItemCard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lift_log_card);
+
+    }
+}
