198d227ae9ba870a687b8120ce6c976dacfab386
Alex <thammavongsa.a@northeastern.edu>
2021-04-17 16:19:07 -0400

    added ids to all textview

--- app/src/main/java/edu/neu/madcourse/gritoria/LiftingStaticView.java
+++ app/src/main/java/edu/neu/madcourse/gritoria/LiftingStaticView.java
@@ -1,6 +1,16 @@
 package edu.neu.madcourse.gritoria;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LiftingStaticView extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lifting_static_view);
+
+
+
+    }
 }
